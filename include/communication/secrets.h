100b48033dff04b04e8e6c8d2189677d48eff247
Jagienka Wójcik <44545551+meladxo@users.noreply.github.com>
2026-07-19 12:43:50 +0200

    secrets.h

---
+++ include/communication/secrets.h
@@ -0,0 +1,17 @@
+/* 
+    Plik konfiguracji kluczy i haseł został zanonimizowany na potrzeby udostępniania.
+*/
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+    // Dane dostępu do kanału thingSpeak; wykorzystanie: thingSpeakManager
+    constexpr char THINGSPEAK_CHANNEL_ID[] = "***";
+    constexpr char writeAPIKey[] = "***";
+
+    // Dane dostępu do sieci WiFi; wykorzystanie: timeManager
+    constexpr char ssid[]     = "***";
+    constexpr char password[] = "***";
+
+
+#endif
